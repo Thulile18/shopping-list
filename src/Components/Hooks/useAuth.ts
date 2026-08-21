97df512d030dbb6ae948fa46abe032e8ab2be5cc
Thulile18 <busisiwegqada0@gmail.com>
2026-08-21 05:23:29 +0200

    Corrected import paths for Store and authSlice

--- src/Components/Hooks/useAuth.ts
+++ src/Components/Hooks/useAuth.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { AppDispatch } from '../store';
+import { AppDispatch } from '../Store';
 import {
   loginUser,
   registerUser,
@@ -11,24 +11,20 @@ import {
   selectAuthLoading,
   selectAuthError,
   selectIsAuthenticated,
-} from '../store/slices/authSlice';
+} from '../Store/authSlice';
 
 export function useAuth() {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
-  
-  // Read our authentication states from our selectors
   const user = useSelector(selectUser);
   const token = useSelector(selectToken);
   const loading = useSelector(selectAuthLoading);
   const error = useSelector(selectAuthError);
   const isAuthenticated = useSelector(selectIsAuthenticated);
 
-  // Simple handler function to sign in a user
   async function login(email: string, password: string) {
     const resultAction = await dispatch(loginUser({ email: email, password: password }));
     
-    // Check if the login dispatch was successful using standard action matching
     if (loginUser.fulfilled.match(resultAction)) {
       navigate('/');
       return { success: true };
@@ -37,7 +33,6 @@ export function useAuth() {
     }
   }
 
-  // Simple handler function to sign up a new account
   async function register(data: {
     name: string;
     surname: string;
@@ -47,7 +42,6 @@ export function useAuth() {
   }) {
     const resultAction = await dispatch(registerUser(data));
     
-    // Check if the signup dispatch was successful using standard action matching
     if (registerUser.fulfilled.match(resultAction)) {
       navigate('/');
       return { success: true };
@@ -56,18 +50,15 @@ export function useAuth() {
     }
   }
 
-  // Simple handler function to sign out and clear states
   function logoutUser() {
     dispatch(logout());
     navigate('/login');
   }
 
-  // Simple helper function to clear global error states
   function clearAuthError() {
     dispatch(clearError());
   }
 
-  // Return all functions and states cleanly inside a standard return object
   return {
     user: user,
     token: token,
