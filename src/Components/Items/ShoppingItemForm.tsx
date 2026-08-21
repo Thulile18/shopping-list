4e6458d7a84374aac0b62cab35a50d0ec03eff5e
Thulile18 <busisiwegqada0@gmail.com>
2026-08-21 06:13:46 +0200

    Fix import statement syntax in ShoppingItemForm

--- src/Components/Items/ShoppingItemForm.tsx
+++ src/Components/Items/ShoppingItemForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { AppDispatch } from '../Store';
 import { addShoppingList, editShoppingList } from '../Store/shoppingSlice';
 import Button from '../Button';
