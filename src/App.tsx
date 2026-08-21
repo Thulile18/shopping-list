0127432b9e824179b04e3f5fd5dd7c64e408cc7e
Thulile18 <busisiwegqada0@gmail.com>
2026-08-21 02:51:45 +0200

    Updated import paths for components and removed commented sections for clarity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { RootState } from './store';
-
-// Import our components
-import Navbar from './components/common/Navbar';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
+import { RootState } from './Components/Store';
+import Navbar from './Components/Navbar';
+import Login from './Components/Pages/Login';
+import Register from './Components/Pages/Register';
+import Home from './Components/Pages/Home';
+import Profile from './Components/Pages/Profile';
 
 function App() {
-  // Check if the user is logged in by looking for the token inside our Redux store
   const { token } = useSelector(function (state: RootState) {
     return state.auth;
   });
@@ -23,7 +20,7 @@ function App() {
       <Navbar />
       
       <Routes>
-        {/* Public Routes: If logged in, send them home automatically */}
+        
         <Route
           path="/login"
           element={isLoggedIn === true ? <Navigate to="/" replace /> : <Login />}
@@ -33,7 +30,6 @@ function App() {
           element={isLoggedIn === true ? <Navigate to="/" replace /> : <Register />}
         />
 
-        {/* Private Routes: If NOT logged in, redirect them back to the login screen */}
         <Route
           path="/"
           element={isLoggedIn === true ? <Home /> : <Navigate to="/login" replace />}
@@ -43,7 +39,6 @@ function App() {
           element={isLoggedIn === true ? <Profile /> : <Navigate to="/login" replace />}
         />
 
-        {/* Catch-all: Send any broken web links back to the home page */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
