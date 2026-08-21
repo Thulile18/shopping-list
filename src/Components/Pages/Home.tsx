00b09f4dd3e4066248a88226cdf3b714565a6675
Thulile18 <busisiwegqada0@gmail.com>
2026-08-21 06:30:10 +0200

    Remove unused React imports from Home.tsx

--- src/Components/Pages/Home.tsx
+++ src/Components/Pages/Home.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectUser } from '../Store/authSlice';
