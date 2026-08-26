e9c5933d7d40f5ede9a2549e51e0c25fb6435f8d
Thulile18 <busisiwegqada0@gmail.com>
2026-08-26 16:08:00 +0200

    Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
     open: true, // Automatically opens the app in the browser when running
   },
 });
