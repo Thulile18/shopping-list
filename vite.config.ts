0a573163a62ccefa2b0093b7e3c8b1172f57833f
Thulile18 <busisiwegqada0@gmail.com>
2026-08-27 08:40:36 +0200

    Fixed the port for Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Define the configuration settings for our Vite compilation engine
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    open: true, // Automatically opens the app in the browser when running
+    open: true, 
   },
 });
